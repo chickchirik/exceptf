878de3b9e4b803bd30446384f985869d5096fef8
yellow duck <orangeboi@yandex.ru>
2019-11-12 16:15:29 +0200

    added Exceptf use-case example

---
+++ example/main.cpp
@@ -0,0 +1,17 @@
+#include "Exceptf.h"
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    int errNum    = 1;
+    std::string errMsg = "ERROR MESSAGE";
+    try {
+        throw Exceptf("EXCEPTF\nerror number: %d\nerror message: %s\nfile: %s\nline: %d\n",
+            errNum, errMsg.c_str(), __FILE__, __LINE__
+        );
+    }
+    catch (Exceptf& error) {
+        std::cout << error.what() << std::endl;
+    }
+    return 0;
+}
